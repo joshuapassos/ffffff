25275534c7e8a9595ac59ef9977d3eba25f5d5c5
Joshua Passos <joshuapassos@gmail.com>
2025-11-09 12:53:07 -0300

    refactor: reorganize Header and HashEntry structs for improved clarity

--- src/storage.rs
+++ src/storage.rs
@@ -1,7 +1,21 @@
 
 struct Header {
   total_size: u64,
+
+  lookup_start: u64,
+
   start_data: u64,
   offset_free: u64,
-  
+}
+
+enum EntryState {
+  Used = 0,
+  Deleted = 1,
+}
+
+struct HashEntry {
+  key: u64,
+  data_offset: u64,
+  size: u64,
+  state: EntryState,
 }
